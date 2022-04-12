917b02afe755aae4bf2cbb29d713a44ed6ac2135
HSBEST13 <89802128+HSBEST13@users.noreply.github.com>
2022-04-12 07:59:02 +0300

    Commit

--- resources/complaint.py
+++ resources/complaint.py
@@ -2,15 +2,10 @@ from data import db_session
 from flask import jsonify
 from data.complaint import Complaint
 from flask_restful import Resource
-from werkzeug.security import generate_password_hash
 
 
 class GetComplaints(Resource):
-    api_key = "AUBWEWE_EWNETWEWE_UBEMUBEM_OSAS_228"
-
-    def get(self, user_id: str, hashed_api_key: str):
-        if generate_password_hash(self.api_key) != hashed_api_key:
-            return jsonify({"error": "wrong-api-key"})
+    def get(self, user_id: str):
         db_sess = db_session.create_session()
         complaints = db_sess.query(Complaint).filter(Complaint.user_id == user_id)
         to_return = {"complaints": []}
